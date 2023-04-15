de09e3b00beaf5a71127e8ced010f127e49c1c27
Siarhei <bordov9635@gmail.com>
2023-04-15 22:45:36 +0300

    commit1

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        String f = String.valueOf(first);
+        String s = String.valueOf(second);
+        String t = String.valueOf(third);
+        System.out.println(f + s + t);
     }
 }
